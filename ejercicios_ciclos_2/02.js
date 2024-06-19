e2e44509c0092b2ccf1913708b6321c4dca19aac
Nicolas_Gallardo <gallardo41299@gmail.com>
2024-06-19 14:07:28 -0300

    Estableciendo las primeras variables del enunciado#

---
+++ ejercicios_ciclos_2/02.js
@@ -0,0 +1,25 @@
+/*
+2)- Crea un script que escriba los números del 1 al 500, que indique cuáles son múltiplos de 4 y de 9 y que
+cada 5 líneas muestre una línea horizontal. Por ejemplo :
+1
+2
+3
+4 (Múltiplo de 4)
+5-
+————————————————————-
+6
+7
+8 (Múltiplo de 4)
+9 (Múltiplo de 9)
+10
+*/
+
+let numeros = 1;
+let resto = 0;
+
+do {
+
+    resto  = numeros 
+    
+} while (numeros === 500);
+
