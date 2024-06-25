f6f42173c31ef8036b385d4f78b28710a9ed3e15
Nicolas_Gallardo <gallardo41299@gmail.com>
2024-06-25 18:24:07 -0300

    Finalización del ejercicio 2

---
+++ ejercicios_array_01/02.js
@@ -0,0 +1,21 @@
+/* Ejercicio 2
+Dada una playlist de canciones definida con un array verificar si contiene la canción 'Ella Baila Sola'
+e imprimir en consola .
+
+const lista = ['Flowers','Kill Bill','As It Was','Seven (feat. Latto)','Ella Baila Sola','Cruel Summer','Creepin’ (with The Weeknd & 21 Savage)','Calm Down (with Selena Gomez)','Shakira: Bzrp Music Sessions, Vol. 53','Anti-Hero']
+*/
+
+const lista = [
+  "Flowers",
+  "Kill Bill",
+  "As It Was",
+  "Seven (feat. Latto)",
+  "Ella Baila Sola",
+  "Cruel Summer",
+  "Creepin’ (with The Weeknd & 21 Savage)",
+  "Calm Down (with Selena Gomez)",
+  "Shakira: Bzrp Music Sessions, Vol. 53",
+  "Anti-Hero",
+];
+
+console.log(lista.includes('Ella Baila Sola'))
