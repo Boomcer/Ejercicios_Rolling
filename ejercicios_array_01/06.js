846ab401ff9a5ae1564305eb97a01f104a9389b5
Nicolas_Gallardo <gallardo41299@gmail.com>
2024-06-26 02:02:30 -0300

    Finalizacion del ejercicio 6

---
+++ ejercicios_array_01/06.js
@@ -0,0 +1,27 @@
+/* Ejercicio 6
+En un colegio hay tres comisiones distintas con 5 alumnos cada una, debido a cambios en los horarios se deben
+unir las comisiones en una sola llamada 'comisionNueva'. Luego imprimir los nombres de los alumnos de la nueva
+comisión.
+*/
+
+const comision1 = ["Santiago Martínez","Valentina Rodríguez","Mateo González","Sofía Fernández","Facundo López"];
+const comision2 = ["Catalina Pérez","Juan García","Camila Díaz","Lucas Gómez","Abril Sánchez"];
+const comision3 = ["Ignacio Rodríguez","Florencia Martín","Matías Herrera","Antonella Pérez","Tomás Romero"];
+
+const comisionNueva = comision1.slice() + comision2.slice() + comision3.slice();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
